49feca4c32fd905d6046d9f666f15bbeeee8a1cc
Mehedi Robin <robinalization@gmail.com>
2026-09-18 07:13:45 +0600

    First Animation

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,14 @@
+gsap.to(".card", {
+  opacity: 1,
+  scale: 1,
+  y: 200,
+  duration: 5,
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -10,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+  },
+});
